059ae8e5ff489d34eadd780e8749252ffae6d2c7
ayeshoali <ayeshoali@gmail.com>
2022-06-10 18:57:57 +0500

    updated again

--- DOM.js
+++ DOM.js
@@ -2,6 +2,7 @@ let className = document.getElementsByClassName("content");
 
 console.log(className);
 console.log(className[0].innerHTML);
+//Changing inner HTML
 //className[0].innerHTML = "<h1> hi content is changed </h1>";
 
 let tag = className[0].getElementsByTagName("h2");
@@ -15,8 +16,33 @@ function changeInnerHTML(tag) {
 
 let id = document.getElementById("intro");
 console.log(id);
-id.textContent = "changed content";
+
+//setting and changing HTML attributes
+id.setAttribute("alt", "hello");
+
+
+
+//id.textContent = "changed content";
 // function timeoutFun() {
 //     timeout = setTimeout(changeInnerHTML(tag[0]), 500000);
 // }
 // timeoutFun();
+
+
+//Changing CSS
+id.setAttribute("style", "color: red");
+id.style.color = "green";
+id.style.backgroundColor = "yellow";
+
+//ADDING ELEMENTS IN HTML
+
+let newA = document.createElement("a");
+
+className[0].appendChild(newA);
+newA.innerHTML = " Link 6";
+newA.setAttribute("href", "a");
+newA.setAttribute("class", "link-6");
+
+
+//REMOVE ELEMENTS
+className[0].removeChild(newA);
---
+++ events.js
@@ -0,0 +1,9 @@
+let elementID = document.getElementById("intro");
+
+elementID.onclick = function() {
+    (alert("clicked"));
+};
+
+// elementID.onmouseover = function() {
+//     (alert("mouse over"));
+// };
