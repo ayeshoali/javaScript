39b239fb63d9363964df4ef6749640777f3f6328
ayeshoali <ayeshoali@gmail.com>
2022-06-13 14:22:30 +0500

    ...

--- strings.js
+++ strings.js
@@ -3,3 +3,6 @@ console.log(introduction);
 console.log("Length of string " + introduction.length);
 console.log(introduction.slice(5));
 console.log(introduction.split(" "));
+
+
+//NEW STRING METHODS
